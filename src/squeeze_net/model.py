1d99711945d99744bbcc207b227a7712df036593
Haislich <josedlv936@gmail.com>
2024-08-02 17:37:20 +0200

    Added until fire7

--- src/squeeze_net/model.py
+++ src/squeeze_net/model.py
@@ -29,13 +29,25 @@ class Fire(nn.Module):
             expand1_dim + expand3_dim
         ), "squeeze1_dim (s1x1), less than expand1_dim + expand3_dim (e1x1 + e3x3)"
 
-        self.squeeze = nn.Conv2d(in_dim, squeeze1_dim, kernel_size=1)
+        self.squeeze = nn.Conv2d(
+            in_channels=in_dim,
+            out_channels=squeeze1_dim,
+            kernel_size=1,
+        )
         self.squeeze_activation = nn.ReLU()
 
-        self.expand1 = nn.Conv2d(squeeze1_dim, expand1_dim, kernel_size=1)
+        self.expand1 = nn.Conv2d(
+            in_channels=squeeze1_dim,
+            out_channels=expand1_dim,
+            kernel_size=1,
+        )
         self.expand1_activation = nn.ReLU()
 
-        self.expand3 = nn.Conv2d(expand1_dim, expand3_dim, kernel_size=3)
+        self.expand3 = nn.Conv2d(
+            in_channels=expand1_dim,
+            out_channels=expand3_dim,
+            kernel_size=3,
+        )
         self.expand3_activation = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -50,4 +62,62 @@ class Fire(nn.Module):
                 self.expand3_activation(self.expand3(x)),
             ],
             dim=1,
+        )
+
+
+class SqueezeNet(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        # Images are assumed to be 224x224x3, WxHxC, and should return a tensor with 96 channels.
+        self.conv1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=96,
+            kernel_size=7,
+            stride=2,
+        )
+        self.conv1_activation = nn.ReLU()
+
+        self.maxpool1 = nn.MaxPool2d(
+            kernel_size=3,
+            stride=2,
+        )
+
+        self.fire2 = Fire(
+            in_dim=96,
+            squeeze1_dim=16,
+            expand1_dim=64,
+            expand3_dim=64,
+        )
+        self.fire3 = Fire(
+            in_dim=128,
+            squeeze1_dim=16,
+            expand1_dim=64,
+            expand3_dim=64,
+        )
+        self.fire14 = Fire(
+            in_dim=128,
+            squeeze1_dim=32,
+            expand1_dim=128,
+            expand3_dim=128,
+        )
+
+        self.maxpool4 = nn.MaxPool2d(kernel_size=3, stride=2)
+
+        self.fire5 = Fire(
+            in_dim=256,
+            squeeze1_dim=32,
+            expand1_dim=128,
+            expand3_dim=128,
+        )
+        self.fire6 = Fire(
+            in_dim=256,
+            squeeze1_dim=48,
+            expand1_dim=192,
+            expand3_dim=192,
+        )
+        self.fire7 = Fire(
+            in_dim=384,
+            squeeze1_dim=48,
+            expand1_dim=192,
+            expand3_dim=192,
         )
